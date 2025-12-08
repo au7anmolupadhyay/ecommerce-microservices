94d0d683c2dbf2fdfa833329e0963000794c5d60
au7anmolupadhyay <anmolupadhau7@gmail.com>
2025-12-08 23:28:06 +0530

    added api gateway hardcoded uri

---
+++ services/api-gateway/src/main/java/com/ecom/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.ecom.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(auth -> auth
+                        .pathMatchers("/auth/**").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/product/**").permitAll()
+                        .pathMatchers("/inventory/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .build();
+    }
+}
